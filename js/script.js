f746b7f2e3184cd278a22db1031ec7f8c6b57681
DmitriyBratishchevAIM <dmitriy.bratishchev@in-aim.ru>
2022-01-17 02:47:21 +0300

    evklid two section

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/* slider */
+
+const swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true
+  },
+  speed: 1000,
+  // effect: "coverflow",
+  // coverflowEffect: {
+    // rotate: 0
+  // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    allowTouchMove: true,
+    loop: true,
+});
+
+// const bullet = document.querySelectorAll('.swiper-pagination-bullet')
+// bullet.forEach(b => b.addEventListener("click", () => {
+//   swiper.autoplay.start()
+// }))
+
+/* tabs */
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     document.querySelectorAll('.work__btn').forEach(function (e) {
+//         e.addEventListener('click', function (e) {
+//             const tab = e.currentTarget.dataset.path;
+//             document.querySelectorAll('.tab-content').forEach(function (e) {
+//                 e.classList.remove('tab-content--active')
+//                 document.querySelector(`[data-target='${tab}']`).classList.add('tab-content--active');
+//             });
+//         });
+
+//         e.addEventListener('click', function (e) {
+//             const tabDefault = e.currentTarget.dataset.default;
+//             document.querySelectorAll('.work__btn').forEach(function (e) {
+//                 e.classList.remove('work__btn--default')
+//                 document.querySelector(`[data-path='${tabDefault}']`).classList.add('work__btn--default');
+//             })
+//         })
+//     });
+// });
+
+// /* accordion */
+
+// $( function() {
+//     $( "#accordion" ).accordion({
+//         collapsible: true,
+//         active: false,
+//     });
+//   } );
+
+// /* burger */
+
+// const burgerBtn = document.querySelector('.burger');
+// const menuClose = document.querySelector('.menu-close');
+// const menuBurger = document.querySelector('.nav');
+
+// burgerBtn.addEventListener('click', () => {
+//     menuBurger.classList.add('burger-active');
+// });
+
+// menuClose.addEventListener('click', () => {
+//     menuBurger.classList.remove('burger-active');
+// });
+
+
